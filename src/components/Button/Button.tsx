64feac43dc966f97e4f45ae137eb9b335a9e1c21
Omar Atta <omaratta212@gmail.com>
2020-10-18 23:03:17 +0200

    Butotn return type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {ButtonProps} from "./ButtonProps";
 import styles from './Button.module.scss';
 
-export const Button = (props: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = (props) => {
 
   return (
     <button className={styles.btnStyle}>{props.text}</button>
