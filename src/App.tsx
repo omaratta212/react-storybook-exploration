d051fb140310bdb846f9d39c2d89167f6b592ef6
Omar Atta <omaratta212@gmail.com>
2020-10-19 17:23:51 +0200

    Add loading states

--- src/App.tsx
+++ src/App.tsx
@@ -5,73 +5,72 @@ import {ButtonSize, ButtonType} from "./components/Button/ButtonProps";
 function App() {
   return (
     <Fragment>
-      <div>
-        <div style={{display: "flex", width: "50%"}}>
-          <Button
-            size={ButtonSize.large}
-            type={ButtonType.primary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.large}
-            type={ButtonType.secondary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.large}
-            type={ButtonType.outlined}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.large}
-            type={ButtonType.text}
-            text={'Button'}
-          />
-        </div>
-        <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
-          <Button
-            size={ButtonSize.medium}
-            type={ButtonType.primary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.medium}
-            type={ButtonType.secondary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.medium}
-            type={ButtonType.outlined}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.medium}
-            type={ButtonType.text}
-            text={'Button'}
-          />
-        </div>
-        <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
-          <Button
-            size={ButtonSize.small}
-            type={ButtonType.primary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.small}
-            type={ButtonType.secondary}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.small}
-            type={ButtonType.outlined}
-            text={'Button'}
-          />
-          <Button
-            size={ButtonSize.small}
-            type={ButtonType.text}
-            text={'Button'}
-          />
-        </div>
+      <h1>Normal</h1>
+      <div style={{display: "flex", width: "50%"}}>
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.primary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.secondary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.outlined}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.text}
+          text={'Button'}
+        />
+      </div>
+      <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.primary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.secondary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.outlined}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.text}
+          text={'Button'}
+        />
+      </div>
+      <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.primary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.secondary}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.outlined}
+          text={'Button'}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.text}
+          text={'Button'}
+        />
       </div>
 
       <h1>Disabled</h1>
@@ -101,7 +100,6 @@ function App() {
           disabled={true}
         />
       </div>
-
       <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
         <Button
           size={ButtonSize.medium}
@@ -128,7 +126,6 @@ function App() {
           disabled={true}
         />
       </div>
-
       <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
         <Button
           size={ButtonSize.small}
@@ -155,6 +152,87 @@ function App() {
           disabled={true}
         />
       </div>
+
+      <h1>Loading</h1>
+      <div style={{display: "flex", width: "50%"}}>
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.primary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.secondary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.outlined}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.large}
+          type={ButtonType.text}
+          text={'Button'}
+          loading={true}
+        />
+      </div>
+      <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.primary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.secondary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.outlined}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.medium}
+          type={ButtonType.text}
+          text={'Button'}
+          loading={true}
+        />
+      </div>
+      <div style={{display: "flex", width: "50%", marginTop: "50px"}}>
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.primary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.secondary}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.outlined}
+          text={'Button'}
+          loading={true}
+        />
+        <Button
+          size={ButtonSize.small}
+          type={ButtonType.text}
+          text={'Button'}
+          loading={true}
+        />
+      </div>
+
     </Fragment>
   );
 }
